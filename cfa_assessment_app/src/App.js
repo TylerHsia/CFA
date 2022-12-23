47dc7fb9cd600b463fcf8f7688fdb7ea3d66215d
Tyler Hsia <TylerHsia@gmail.com>
2022-12-23 11:21:29 -0800

    searches all poems with given title, displays info

--- cfa_assessment_app/src/App.js
+++ cfa_assessment_app/src/App.js
@@ -3,70 +3,68 @@ import './App.css';
 import React, { useState, useEffect } from 'react';
 
 function App() {
-  function searchClick(){
-    console.log(document.getElementById("search-box").value);
-  }
-  console.log("hello world");
-
-  useEffect(() => {
-    console.log("loaded");
-    document.getElementById("display-text").innerHTML = "loading";
-    searchBox = document.getElementById("search-box");
-    console.log(searchBox.value);
-  }, [""]);
-
-  let searchBox;
-  useEffect(() => {
-    console.log("hello");
-    console.log(searchBox.value);
-
-
-  }, searchBox);
-
-
 
   // create a new XMLHttpRequest
-  var xhr = new XMLHttpRequest()
-  xhr.responseType = '';
+  var xhr = new XMLHttpRequest();
+  function searchClick() {
+    document.getElementById("display-text").innerHTML = "loading";
+    let searchBox = document.getElementById("search-box");
+    console.log(searchBox.value);
+    // get a callback when the server responds
+    xhr.addEventListener("load", () => {
+      // update the state of the component with the result here
+      // console.log(xhr.response);
+      let result = JSON.parse(xhr.response);
+      console.log(result);
+      document.getElementById("display-text").innerHTML = "";
+      if (result?.length) {
+        result.map(poem => console.log(poem.linecount));
+        let displayText = document.getElementById("display-text");
+        for (let poem of result) {
+          let title = document.createElement("p");
+          title.innerHTML = poem.title;
+          displayText.appendChild(title);
+
+          let author = document.createElement("p");
+          author.innerHTML = "By " + poem.author;
+          displayText.appendChild(author);
+
+          let lines = document.createElement("p");
+          lines.innerHTML = poem.lines.join("<br>");
+          displayText.appendChild(lines);
+          console.log(poem);
+        }
+
+        document.getElementById("results-count").innerHTML = result.length + " poem" + (result.length == 1 ? " was" : "s were") + " found with this title";
+      } else {
+        document.getElementById("results-count").innerHTML = "No poems were found with this title";
+      }
 
 
+    })
+    // open the request with the verb and the url
+    xhr.open("GET", "https://poetrydb.org/title/" + searchBox.value);
+    // send the request
+    xhr.send();
+  }
 
-  // get a callback when the server responds
-  xhr.addEventListener("load", () => {
-    // update the state of the component with the result here
-    // console.log(xhr.response);
-    document.getElementById("display-text").innerHTML = JSON.parse(xhr.response)[0]["lines"];
-  })
-  // open the request with the verb and the url
-  xhr.open("GET", "https://poetrydb.org/author,linecount/Shakespeare;14/lines")
-  // send the request
-  xhr.send()
   return (
     <div className="App">
-      <div id="display-div">
-        <p id="display-text">
-          hello world
-        </p>
-        <input type="text" placeholder='type here' id="search-box"></input>
-      </div>
+      <input type="text" placeholder='type here' id="search-box"></input>
+
       <button id="search-button" type="button" onClick={() => searchClick()}>
         <i className="fa fa-search" aria-hidden="true"></i>
       </button>
-
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p id="results-count">
+        no search has been performed
+      </p>
+      <div id="display-div">
+        <div id="display-text">
+          <p>
+            Please search something
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
