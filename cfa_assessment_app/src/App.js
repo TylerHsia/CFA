8232dc6ac56453d4bf7540d5ce3440989b5ddad6
Tyler Hsia <TylerHsia@gmail.com>
2022-12-27 16:16:19 -0800

    Added authorSelection functionality

--- cfa_assessment_app/src/App.js
+++ cfa_assessment_app/src/App.js
@@ -5,56 +5,145 @@ import React, { useState, useEffect } from 'react';
 function App() {
 
   // create a new XMLHttpRequest
-  var xhr = new XMLHttpRequest();
+
+  let authors;
+  let selectAuthorText = "Please select an author";
+
   function searchClick() {
-    document.getElementById("display-text").innerHTML = "loading";
+    var xhr = new XMLHttpRequest();
+
     let searchBox = document.getElementById("search-box");
     console.log(searchBox.value);
-    // get a callback when the server responds
-    xhr.addEventListener("load", () => {
-      // update the state of the component with the result here
-      // console.log(xhr.response);
-      let result = JSON.parse(xhr.response);
-      console.log(result);
-      document.getElementById("display-text").innerHTML = "";
-      if (result?.length) {
-        result.map(poem => console.log(poem.linecount));
-        let displayText = document.getElementById("display-text");
-        for (let poem of result) {
-          let title = document.createElement("p");
-          title.innerHTML = poem.title;
-          displayText.appendChild(title);
 
-          let author = document.createElement("p");
-          author.innerHTML = "By " + poem.author;
-          displayText.appendChild(author);
+    if (searchBox.value) {
+      let authorSet = new Set();
+      document.getElementById("display-text").innerHTML = "loading";
+      // get a callback when the server responds
+      xhr.addEventListener("load", () => {
+        // update the state of the component with the result here
+        // console.log(xhr.response);
+        let result = JSON.parse(xhr.response);
+        console.log(result);
+        document.getElementById("display-text").innerHTML = "";
+        if (result?.length) {
+          result.map(poem => console.log(poem.linecount));
+          let displayText = document.getElementById("display-text");
+          for (let poem of result) {
+            let title = document.createElement("p");
+            title.innerHTML = poem.title;
+            displayText.appendChild(title);
 
-          let lines = document.createElement("p");
-          lines.innerHTML = poem.lines.join("<br>");
-          displayText.appendChild(lines);
-          console.log(poem);
+            let author = document.createElement("p");
+            author.innerHTML = "By " + poem.author;
+            authorSet.add(poem.author);
+            displayText.appendChild(author);
+
+
+            let lines = document.createElement("p");
+            lines.innerHTML = poem.lines.join("<br>");
+            displayText.appendChild(lines);
+            console.log(poem);
+          }
+
+          authors = Array.from(authorSet).sort();
+          console.log(authors);
+
+          let authorSelect = document.getElementById("author-select");
+          authorSelect.innerHTML = "";
+
+          let defaultOption = document.createElement("option");
+          defaultOption.innerHTML = selectAuthorText;
+          authorSelect.appendChild(defaultOption);
+          for(let author of authors){
+            let authorOption = document.createElement("option");
+            authorOption.innerHTML = author;
+            authorSelect.appendChild(authorOption);
+          }
+
+          document.getElementById("results-count").innerHTML = result.length + " poem" + (result.length == 1 ? " was" : "s were") + " found with this title";
+
+
+        } else {
+          document.getElementById("results-count").innerHTML = "No poems were found with this title";
         }
-
-        document.getElementById("results-count").innerHTML = result.length + " poem" + (result.length == 1 ? " was" : "s were") + " found with this title";
-      } else {
-        document.getElementById("results-count").innerHTML = "No poems were found with this title";
-      }
+        
 
 
-    })
-    // open the request with the verb and the url
-    xhr.open("GET", "https://poetrydb.org/title/" + searchBox.value);
-    // send the request
-    xhr.send();
+        // xhr.open("GET", "https://poetrydb.org/title,author/" + searchBox.value + ";" + Array.from(authors).sort()[0]);
+        // xhr.send();
+      })
+      // open the request with the verb and the url
+      xhr.open("GET", "https://poetrydb.org/title/" + searchBox.value);
+      // send the request
+      xhr.send();
+    }
   }
+
+  function authorSelect(e) {
+    var xhr = new XMLHttpRequest();
+    let searchBox = document.getElementById("search-box");
+    console.log(e);
+    console.log(e.target.value);
+    if (searchBox.value && e?.target?.value && e.target.value !== selectAuthorText) {
+      document.getElementById("display-text").innerHTML = "loading";
+      // get a callback when the server responds
+      xhr.addEventListener("load", () => {
+        // update the state of the component with the result here
+        // console.log(xhr.response);
+        let result = JSON.parse(xhr.response);
+        console.log(result);
+        document.getElementById("display-text").innerHTML = "";
+        if (result?.length) {
+          result.map(poem => console.log(poem.linecount));
+          let displayText = document.getElementById("display-text");
+          for (let poem of result) {
+            let title = document.createElement("p");
+            title.innerHTML = poem.title;
+            displayText.appendChild(title);
+
+            let author = document.createElement("p");
+            author.innerHTML = "By " + poem.author;
+            displayText.appendChild(author);
+
+
+            let lines = document.createElement("p");
+            lines.innerHTML = poem.lines.join("<br>");
+            displayText.appendChild(lines);
+            console.log(poem);
+          }
+
+          console.log(authors);          
+
+          document.getElementById("results-count").innerHTML = result.length + " poem" + (result.length == 1 ? " was" : "s were") + " found with this title";
+
+
+        } else {
+          document.getElementById("results-count").innerHTML = "No poems were found with this title";
+        }
+        
+
+
+      })
+      // open the request with the verb and the url
+      xhr.open("GET", "https://poetrydb.org/title,author/" + searchBox.value + ";" + e.target.value);
+      // send the request
+      xhr.send();
+    }
+  }
+
+
 
   return (
     <div className="App">
+      <h1>Title</h1>
       <input type="text" placeholder='type here' id="search-box"></input>
 
       <button id="search-button" type="button" onClick={() => searchClick()}>
         <i className="fa fa-search" aria-hidden="true"></i>
       </button>
+      <select class="form-select" aria-label="Default select example" id="author-select" onChange={(e) => authorSelect(e)}>
+        <option selected>Make a search to enable author selection</option>
+      </select>
       <p id="results-count">
         no search has been performed
       </p>
